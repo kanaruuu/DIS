80ba3a67e9b959e5a604131aa2268d614731f993
Konstantin <113326693+kanaruuu@users.noreply.github.com>
2024-12-25 14:30:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,57 @@
 class Controller:
     def __init__(self, elevs):
-        self.elevs = elevs  
+        self.elevs = elevs
+        self.pending_requests = []
 
     def elv_choose(self, pickup_floor):
-        # Выбираем свободный ближайший лифт 
-
-
         elevs_distances = [
-            (self.elevs[0], abs(self.elevs[0].current_floor - pickup_floor)),
-            (self.elevs[1], abs(self.elevs[1].current_floor - pickup_floor))
+            (elev, abs(elev.current_floor - pickup_floor)) for elev in self.elevs
         ]
 
-        # Фильтруем занятые и выбираем ближайший свободный если есть
-        free_elevs = [elev for elev, distance in elevs_distances if not elev.is_busy()]
-        nearest_elv = min(free_elevs, key=lambda elev: abs(elev.current_floor - pickup_floor), default=None)
-        nearest_elv and nearest_elv.set_busy(True)
+        free_elevs = filter(lambda elev_dist: not elev_dist[0].is_busy(), elevs_distances)
+        nearest_elv = min(free_elevs, key=lambda elev_dist: elev_dist[1], default=None)
 
-        return nearest_elv
+        nearest_elv and nearest_elv[0].set_busy(True)
+
+        return nearest_elv[0] if nearest_elv else None
+
     def request(self, pickup_floor, target_floor):
         chosen_elv = self.elv_choose(pickup_floor)
-        self.move_elv(chosen_elv, pickup_floor)
+        
+        (lambda: chosen_elv.add_request(pickup_floor, target_floor), 
+         lambda: self.pending_requests.append((pickup_floor, target_floor)))[chosen_elv is None]()
 
-        chosen_elv.open_doors()
-        chosen_elv.close_doors()
-
-        self.move_elv(chosen_elv, target_floor)
-
-        chosen_elv.open_doors()
-        chosen_elv.close_doors()
-
-        return chosen_elv.movement_count, chosen_elv
-
-
-
-    def move_elv(self, elevator, target_floor):
-        elevator.set_target(target_floor)
-        elevator.execute_movement()
+    def process_pending_requests(self):
+        list(map(lambda elev: self.pending_requests and not elev.is_busy() and elev.add_request(*self.pending_requests.pop(0)), self.elevs))
 
 class Elev:
     def __init__(self, current_floor, floor_dispatcher):
         self.current_floor = current_floor
-        self.floor_dispatcher = floor_dispatcher  
+        self.floor_dispatcher = floor_dispatcher
         self.movement_count = 0
         self.command_log = []
         self.busy = False
+        self.requests = []
         self.direction = None
         self.target_floor = None
-        
-        self.movement_actions = {
-            'up': self.move_up,
-            'down': self.move_down
-        }
+
+    def add_request(self, pickup_floor, target_floor):
+        self.requests.append((pickup_floor, target_floor))
+        self.process_requests()
 
     def set_target(self, target_floor):
-        # Задаем нужный этаж, проверяем с помощью FloorDispatcher, потом расчитываем направление 
-        self.target_floor = self.floor_dispatcher.get_floor(target_floor)  # Проверка допустимости этажа
-        self.direction = 'up' if self.current_floor < self.target_floor else 'down'
+        self.target_floor = self.floor_dispatcher.get_floor(target_floor)
+        self.direction = ('up', 'down')[self.current_floor > self.target_floor]
 
     def move_up(self):
         next_floor = self.current_floor + 1
-        self.current_floor = self.floor_dispatcher.get_floor(next_floor)  # Проверка через диспетчер
+        self.current_floor = self.floor_dispatcher.get_floor(next_floor)
         self.movement_count += 1
         self.command_log.append(f"Подъем на {self.current_floor} этаж")
 
     def move_down(self):
         next_floor = self.current_floor - 1
-        self.current_floor = self.floor_dispatcher.get_floor(next_floor) 
+        self.current_floor = self.floor_dispatcher.get_floor(next_floor)
         self.movement_count += 1
         self.command_log.append(f"Спуск на {self.current_floor} этаж")
 
@@ -76,9 +62,26 @@ class Elev:
         self.command_log.append("Закрыть двери")
 
     def execute_movement(self):
-        # Исполняем команду движения в направлении до нужного этажа
         while self.current_floor != self.target_floor:
-            self.movement_actions[self.direction]()
+            self.movement_actions()[self.direction]()
+
+    def process_requests(self):
+        self.set_busy(True)
+        while self.requests:
+            pickup_floor, target_floor = self.requests.pop(0)
+            self.set_target(pickup_floor)
+            self.execute_movement()
+            self.open_doors()
+            self.close_doors()
+
+            self.set_target(target_floor)
+            self.execute_movement()
+            self.open_doors()
+            self.close_doors()
+        self.set_busy(False)
+
+    def movement_actions(self):
+        return {'up': self.move_up, 'down': self.move_down}
 
     def get_commands(self):
         return "\n".join(self.command_log)
@@ -89,51 +92,41 @@ class Elev:
     def set_busy(self, state):
         self.busy = state
 
-
-
-
-
 class House:
     def __init__(self, num_floors, elevs_positions):
         self.num_floors = num_floors
-        self.floor_dispatcher = FloorDispatcher(num_floors)  # Создаем диспетчер этажей
+        self.floor_dispatcher = FloorDispatcher(num_floors)
         self.elevs = [Elev(pos, self.floor_dispatcher) for pos in elevs_positions]
         self.controller = Controller(self.elevs)
 
     def process_request(self, pickup_floor, target_floor):
-        moves, elevator = self.controller.request(pickup_floor, target_floor)
-        return moves, elevator
-
+        self.controller.request(pickup_floor, target_floor)
+        self.controller.process_pending_requests()
 
 class InvalidFloorError(Exception):
     pass
-
 
 class FloorDispatcher:
     def __init__(self, max_floor):
         self.valid_floors = {i: i for i in range(1, max_floor + 1)}
 
     def get_floor(self, floor):
-        # Возвращает этаж или выбрасывает исключение
-        try:
-            return self.valid_floors[floor]  
-        except KeyError:
-            raise InvalidFloorError(f"Этаж {floor} недопустим.")
+        return self.valid_floors.get(floor) or self._invalid_floor(floor)
 
-
+    def _invalid_floor(self, floor):
+        raise InvalidFloorError(f"Этаж {floor} недопустим.")
 
 if __name__ == "__main__":
     max_floor = 10
     elevs_positions = [2, 6]  
-    
+
     building = House(max_floor, elevs_positions)
-    
-    queries = [(3, 10), (8, 4)] 
+
+    queries = [(3, 10), (8, 4), (2, 5), (7, 1), (1, 9)]  
+    for pickup_floor, target_floor in queries:
+        building.process_request(pickup_floor, target_floor)
+
     for idx, elevator in enumerate(building.elevs):
-        pickup_floor, target_floor = queries[idx]
-        moves, chosen_elv = building.process_request(pickup_floor, target_floor)
-        print(f"Лифт совершил {moves} команд перемещения до открытия дверей на {target_floor} этаже")
-        print(f"Начальная позиция лифта {idx + 1}: {elevs_positions[idx]} этаж")
         print(f"История команд лифта {idx + 1}:")
         print(elevator.get_commands())
         print()
